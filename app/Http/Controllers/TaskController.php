221c04fca8b2c14350a8d1a29b273080b1fa7e97
Facundo Dellanegra <facundodiciembre25@gmail.com>
2024-05-05 16:44:54 -0300

    first commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $tasks['tasks'] = Task::all(); // Almacena todos los registros en el arreglo de task
+        return view('task.index', $tasks); // Se lo paso a la vista
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $task = request()->all(); // Recibo toda la info enviada desde el formulario (es como si fuese un $_POST)
+        Task::create($task); // Inserto en la base de datos, mediante el modelo, los datos recibidos
+        return redirect('/');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(task $task)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(task $task)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, task $task)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        $task = Task::findOrFail($id);
+        $task->delete();
+        return redirect('/');
+    }
+}
